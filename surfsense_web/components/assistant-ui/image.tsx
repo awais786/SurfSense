5430b3d2d566c7f291202dc77a6e10ae6cb88600
awais786 <awais.qureshi@arbisoft.com>
2026-04-03 12:55:53 +0500

    feat: Adding oauth2-proxy+cognito+oidc.

--- surfsense_web/components/assistant-ui/image.tsx
+++ surfsense_web/components/assistant-ui/image.tsx
@@ -224,4 +224,4 @@ Image.Preview = ImagePreview;
 Image.Filename = ImageFilename;
 Image.Zoom = ImageZoom;
 
-export { Image, ImageRoot, ImagePreview, ImageFilename, ImageZoom, imageVariants };
+export { Image, ImageFilename, ImagePreview, ImageRoot, ImageZoom, imageVariants };
